1e955677c207d8f058ef0cb8c7c6f0b9d0da52bc
Suresh20155 <a.suresh@vit.ac.in>
2025-06-18 12:49:12 +0530

    Add files via upload

---
+++ prg22.ts
@@ -0,0 +1,10 @@
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+// Example usage
+const num1 = 10;
+const num2 = 20;
+const result = sum(num1, num2);
+
+console.log(`The sum of ${num1} and ${num2} is:`, result);
